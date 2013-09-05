797c029b9886e2ea9330b695a5def6b7e7da9fa2
Michel Kenneth Hansen <michelhansen@disruptx.dk>
2013-09-05 23:36:00 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnlightmentProd::Application.config.secret_token = 'c19121391584f0b92d2e81e957cbdace00b2d348140f567a3edba098f203fc1ff9522d723da6ceb92e632ca3f9ae528037101ebfe5a2382ce4ca68d8236a9271'
