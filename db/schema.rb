11b249be0bc8b685620117222ee7b9e2bd8dff54
Michel Kenneth Hansen <michelhansen@disruptx.dk>
2013-09-09 11:45:41 +0200

    sorcery install init

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130908172736) do
+ActiveRecord::Schema.define(:version => 20130908194043) do
 
   create_table "business_hours", :force => true do |t|
     t.integer  "day"
@@ -167,6 +167,22 @@ ActiveRecord::Schema.define(:version => 20130908172736) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+  end
+
+  add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token"
 
   create_table "welcome_offers", :force => true do |t|
     t.boolean  "active"
