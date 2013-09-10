16a3840f5a2293d5b91815c69e6a0d308e18ed4d
Michel Kenneth Hansen <michelhansen@disruptx.dk>
2013-09-10 13:46:26 +0200

    heroku error check

--- app/models/backend_admin.rb
+++ app/models/backend_admin.rb
@@ -1,5 +1,5 @@
 class BackendAdmin < ActiveRecord::Base
   attr_accessible :admin, :name, :role
 
-  #has_one :user, as: => :sub
+  has_one :user, as: => :sub
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ActiveRecord::Base
   attr_accessible :birthday, :gender, :name, :phone, :postal_code
 
-  #has_one :user, as: => :sub
+  has_one :user, as: => :sub
 
 end
