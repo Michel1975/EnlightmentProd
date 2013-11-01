dbde0e1272d5740cbc18767e6fd9efb266cf1c24
Michel Kenneth Hansen <michelhansen@disruptx.dk>
2013-11-01 10:17:24 +0100

    fixed bug in upload with cache

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-  attr_accessible :picture, :size
+  attr_accessible :picture, :size, :picture_cache
   belongs_to :imageable, :polymorphic => true
   mount_uploader :picture, ImageUploader
 end
