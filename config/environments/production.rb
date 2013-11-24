0eba530b2b32e5c397808198d4041161d493f9a6
Michel Kenneth Hansen <michelhansen@disruptx.dk>
2013-11-24 20:44:57 +0100

    added fix to precompile settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,9 @@ EnlightmentProd::Application.configure do
 
   
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( admin_scripts.js admin_styles.css merchant_scripts.js merchant_styles.css )
+  config.assets.precompile += %w( admin/admin_scripts.js admin/admin_styles.css merchant/merchant_scripts.js merchant/merchant_styles.css )
+  #config.assets.precompile << 'admin_scripts.js'
+  
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
